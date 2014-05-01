3a761098c5f1ee2d2b75c419c32d19cf8068cf64
Jim Vitek <jim@appkey.com>
2014-05-01 18:19:03 -0400

    Update to SDK v1.6 Cleaned up resources, changed icon, and general housekeeping

--- src/com/appkey/example/MainActivity.java
+++ src/com/appkey/example/MainActivity.java
@@ -11,21 +11,20 @@ import android.widget.TextView;
 import android.app.Activity;
 
 /**
- * MainActivity of the AppKey Example project
+ * AppKey SDK Demo Integration
  * 
- * The purpose of this project is to provide an AppKey integration example.  This simple activity
- * simulates the hub activity of your application.  The AppKey check is patterned after the Google
- * License Verification Library (LVL), but less complicated and thus simpler to implement.
+ * The purpose of this project is to provide an open source example of an AppKey integration example.  This simple activity
+ * simulates the main activity of your application.  The AppKey check is automatically refreshed onResume(). The result returns
+ * via callback with methods patterned after the Google LVL, but less complicated and simpler to implement.
  *  
  * @author jimvitek
  */
 public class MainActivity extends Activity { 
 
-    private TextView mTextViewAppKeyCheckResult;
+    private TextView mTextViewAppKeyStatus;
     private ImageView mImageViewKeylock;
     private Button mButtonPromptUser;
-    private Button mButtonOpenAppKey;
-    private AppKeyChecker mAppKeyChecker;
+    private AppKeyChecker mAppKeyChecker; 
 
     @Override
     public void onCreate(Bundle savedInstanceState) { 
@@ -33,12 +32,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main); 
 
         mImageViewKeylock = (ImageView)findViewById(R.id.imageViewKeylock);
-        mTextViewAppKeyCheckResult = (TextView)findViewById(R.id.textViewAppKeyCheckResult);
+        mTextViewAppKeyStatus = (TextView)findViewById(R.id.textViewAppKeyCheckResult);
         mButtonPromptUser = (Button)findViewById(R.id.buttonPromptUser);
         mButtonPromptUser.setOnClickListener(ButtonPromptUserListener);
-        mButtonOpenAppKey = (Button)findViewById(R.id.buttonOpenAppKey);
-        mButtonOpenAppKey.setOnClickListener(ButtonOpenAppKeyListener);
-
 
         /* AppKeyChecker - Check whether or not AppKey is enabled
          * 
@@ -46,17 +42,19 @@ public class MainActivity extends Activity {
          * 
 	     * @param context a Context
 	     * @param appID AppKey.com assigned AppID for this app
-	     * @param analyticsEnabled if true, and if the calling app has INTERNET permission, the SDK will send user interactions
+	     * @param analyticsEnabled if true, and if the calling app has INTERNET permission, the SDK will send up to 6 user interaction
 	     *        events along the installation funnel for the purpose of measuring & optimizing conversion. Events are tracked 
-	     *        via Google Analytics using an anonymous UDID.
+	     *        via a Google Analytics compliant anonymous UDID. This data will only be shared with you, the developer of the app.
+	     *        NOTE - The SDK will check for INTERNET permission before sending, and will not bomb if it is not available
+	     *        If false, no tracking data will be sent to AppKey.
         */
-       mAppKeyChecker = new AppKeyChecker(MainActivity.this, "7", true);  //TODO: Replace 7 with your appId from AppKey.com
+        mAppKeyChecker = new AppKeyChecker(MainActivity.this, "7", true);  //TODO: Replace the 7 with your appId from AppKey.com
     }
     
     @Override
-    protected void onResume() {  
+    protected void onResume() {
         super.onResume();
-        mTextViewAppKeyCheckResult.setText("Check in progress");
+        mTextViewAppKeyStatus.setText("Check in progress");
         mImageViewKeylock.setImageResource(R.drawable.appkey_squarekey_gray);
 
         /* 
@@ -71,21 +69,31 @@ public class MainActivity extends Activity {
         @Override
         public void allow() { 
             /*
-             * Insert code to enable premium functionality for this session below
+             * Insert code to enable premium content here
              */
-            mTextViewAppKeyCheckResult.setText("App Unlocked");
-            mImageViewKeylock.setImageResource(R.drawable.appkey_squarekey_green);
-            mButtonPromptUser.setEnabled(false);
+        	
+        	MainActivity.this.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+		            mTextViewAppKeyStatus.setText("App Unlocked");
+		            mImageViewKeylock.setImageResource(R.drawable.appkey_squarekey_green);
+				}
+        	});
         }
 
         @Override
         public void dontAllow(int reason) { 
             /*
-            * Insert code to disable premium functionality for this session below
+            * Insert code to disable premium content here
             */
-            mTextViewAppKeyCheckResult.setText("App Locked");
-            mImageViewKeylock.setImageResource(R.drawable.appkey_squarekey_red);
-            mButtonPromptUser.setEnabled(true);
+        	
+        	MainActivity.this.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+		            mTextViewAppKeyStatus.setText("App Locked");
+		            mImageViewKeylock.setImageResource(R.drawable.appkey_squarekey_red);
+				}
+        	});
         }
     }
     
@@ -99,18 +107,7 @@ public class MainActivity extends Activity {
 		     * @param activity an Activity
 		     * @param benefit Description of what will be unlocked for AppKey users
 		     */
-			mAppKeyChecker.promptUser(MainActivity.this, "[Awesome features]");
-		}
-	};
-
-    View.OnClickListener ButtonOpenAppKeyListener = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-
-		    /**
-		     * Open the AppKey app so user can see cross promotions and local deals (depending on geography)
-		     */
-			mAppKeyChecker.openAppKey();
+			mAppKeyChecker.promptUser(MainActivity.this, "[your awesome feature]");
 		}
 	};
 }
